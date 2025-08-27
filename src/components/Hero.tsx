f03e978a59e73c9bbb8302846cc8eaa5e7738f07
Busra Mestan <bmestan50@gmail.com>
2025-08-27 23:04:59 +0300

    feat: added Hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,9 +1,17 @@
-
+import HeroImageContainer from "./HeroImageContainer";
+import HeroTextContainer from "./HeroTextContainer";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section className="relative bg-gradient-to-r from-orange-50 to-real-50 py-20">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 ">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 items-center">
+          <HeroTextContainer />
+          <HeroImageContainer />
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
---
+++ src/components/HeroImageContainer.tsx
@@ -0,0 +1,25 @@
+import { Heart } from "lucide-react";
+
+const HeroImageContainer = () => {
+  return (
+    <div className="relative">
+      <img
+        src="https://images.pexels.com/photos/1661179/pexels-photo-1661179.jpeg?auto=compress&cs=tinysrgb&w=800"
+        alt="Papağan"
+        className="rounded-3xl shadow-2xl"
+      />
+
+      <div className="absolute -bottom-6 -left-6 bg-white p-4 rounded-3xl shadow-xl">
+        <div className="flex items-center gap-3 ">
+          <Heart className="text-orange-600" />
+          <div>
+            <p className="font-semibold text-gray-900">Uzman Bakım</p>
+            <p className="text-sm text-gray-600">7+ yıl deneyim</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroImageContainer;

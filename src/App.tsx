0f4be7eced5483f1a05172e6b43a2de59e27fe37
Busra Mestan <bmestan50@gmail.com>
2025-08-27 22:15:24 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import CTA from "./components/CTA";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Services from "./components/Services";
+
+const App = () => {
+  return (
+    <div className="min-h-screen bg-gray50">
+      <Header />
+      <main>
+        <Hero />
+        <Services />
+        <CTA />
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
